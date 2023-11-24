d7e400328b096104aec2221b51556cecd56cf011
tawakuliKH <tawakuli456@gmail.com>
2023-11-25 00:03:21 +0330

    Add CoinsSlice

---
+++ src/redux/coins/CoinsSlice.js
@@ -0,0 +1,40 @@
+/* eslint-disable no-param-reassign */
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import axios from 'axios';
+
+const initialState = {
+  coins: [],
+  isLoading: false,
+  errorMessage: '',
+};
+const URL = 'https://api.coincap.io/v2/assets';
+
+export const getCoins = createAsyncThunk('coinsGenere/getCoins', async () => {
+  try {
+    const response = await axios.get(URL);
+    return response.data;
+  } catch (error) {
+    return error;
+  }
+});
+const coinsSlice = createSlice({
+  name: 'coins',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(getCoins.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(getCoins.fulfilled, (state, action) => {
+        state.isLoading = false;
+        const { data } = action.payload;
+        state.coins = data;
+      })
+      .addCase(getCoins.rejected, (state, action) => {
+        state.isLoading = false;
+        state.errorMessage = action.payload;
+      });
+  },
+});
+
+export default coinsSlice.reducer;
